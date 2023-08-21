81329f22e5586624f44cae1e14fe83b0e5a048da
Crowdbotics <team@crowdbotics.com>
2023-08-21 01:16:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "reelo-42971",
+  url: "https://reelo-42971.botics.co",
+  api: "https://reelo-42971.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
